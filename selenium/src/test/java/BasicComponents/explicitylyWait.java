7630fbc61e8fa0d2cfd1a7c8759a7adf89ee6b55
Walter <walter.zhangfang@gmail.com>
2024-03-09 09:46:07 +1300

    create the explicityWait method

--- selenium/src/test/java/BasicComponents/explicitylyWait.java
+++ selenium/src/test/java/BasicComponents/explicitylyWait.java
@@ -1,7 +1,11 @@
 package BasicComponents;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class explicitylyWait {
@@ -10,6 +14,14 @@ public class explicitylyWait {
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
         // Define the maximum time to wait for a particular element
-        WebDriverWait wait = new WebDriverWait(driver, 5); // 5 seconds
+        WebDriverWait wait = new WebDriverWait(driver, null);
+
+        try {
+            WebElement boxElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("autocomplete")));
+            boxElement.sendKeys("New Zealand");
+        } catch (TimeoutException e) {
+            // TODO: handle exception
+            System.out.println("TimeoutException caught: " + e.getMessage());
+        }
     }
 }
