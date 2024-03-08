7df6c981cb3d55aa224ba84f136eb6a1d16803c3
Walter <walter.zhangfang@gmail.com>
2024-03-09 10:41:25 +1300

    create logintest

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 E2E Web UI Automation Testing Framework
 
 
-Using this [website](https://rahulshettyacademy.com/AutomationPractice/) for practising.
-And using this [Web Form](https://www.selenium.dev/selenium/web/web-form.html) for testing the form.
+Elements: [website](https://rahulshettyacademy.com/AutomationPractice/) for practising.
+
+Web form: [Web Form](https://www.selenium.dev/selenium/web/web-form.html) for testing the form.
+
+Login Verification: [Orange HRM](https://opensource-demo.orangehrmlive.com/web/index.php/auth/login)
+
+Admin Login: [nopCommerce](https://admin-demo.nopcommerce.com/login)
--- selenium/src/test/java/BasicComponents/LoginTest.java
+++ selenium/src/test/java/BasicComponents/LoginTest.java
@@ -1,25 +1,53 @@
 package BasicComponents;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.testng.Assert;
+
 public class LoginTest {
     public static void main(String[] args) {
-        System.out.println("LoginTest");
+        // Setup the Chrome web driver
         WebDriver driver = new ChromeDriver();
+
+        // Open the URL
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         try {
-            Thread.sleep(5000);
+            Thread.sleep(5000); // Wait for 5 seconds
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        driver.manage().window().maximize(); // Maximize the window
 
+        // Input the username and password
         driver.findElement(By.name("username")).sendKeys("Admin");
         driver.findElement(By.name("password")).sendKeys("admin123");
 
+        // Click the login button
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+        try {
+            Thread.sleep(5000); // Wait for 5 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //Verify the title of the page
+        Assert.assertEquals("OrangeHRM", driver.getTitle());
 
+        // Verify the lable name
+        String act_lable = null;
+        try {
+            act_lable = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[1]/span/h6")).getText();
+            Assert.assertEquals("Dashboard", act_lable);
+            System.out.println("Lable is correct");
+        } catch (NoSuchElementException e) {
+            // TODO: handle exception
+            act_lable = "";
+            System.out.println("Lable is not correct");
+        }
+
+        // Quit the WebDriver session
         driver.quit();
     }
 
