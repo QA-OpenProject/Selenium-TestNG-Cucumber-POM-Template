f82f0418112c0fc0513af3ee5c9811bfdc992de6
Walter <walter.zhangfang@gmail.com>
2024-03-08 16:22:16 +1300

    update readme.md

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ E2E Web UI Automation Testing Framework
 
 
 Using this [website](https://rahulshettyacademy.com/AutomationPractice/) for practising.
+And using this [Web Form](https://www.selenium.dev/selenium/web/web-form.html) for testing the form.
--- selenium/src/test/java/BasicComponents/launchWeb.java
+++ selenium/src/test/java/BasicComponents/launchWeb.java
@@ -1,6 +1,4 @@
 package BasicComponents;
-import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,18 +8,22 @@ import org.openqa.selenium.By;
 
 public class launchWeb {
      public static void main(String[] args) {
+        // Setup the WebDriver using WebDriverManager
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         
+        // Open the URL
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
         System.out.println("Title of the page: " + driver.getTitle());
 
+        // Implicit wait for 500ms
         driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
 
+        // Find the element by ID and send the keys
         WebElement textBox = driver.findElement(By.id("autocomplete"));
         textBox.sendKeys("New Zealand");
 
-
+        // Quit the WebDriver session
         driver.quit();
     }
 }
