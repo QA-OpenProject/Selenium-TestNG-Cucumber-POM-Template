a80a19e756ed8397419fe614198f290dd484cac4
Fang Zhang <walter.zhangfang@gmail.com>
2024-03-11 09:25:53 +1300

    create the dropdowns

---
+++ selenium/src/test/java/BasicComponents/dropdowns.java
@@ -0,0 +1,53 @@
+package BasicComponents;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+
+public class dropdowns {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		WebElement dropdownElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@id='dropdown-class-example']")));
+		Select dropdown = new Select(dropdownElement);
+		
+		// Select by visible text
+		dropdown.selectByVisibleText("Option2");
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+		/*
+		 * try { Thread.sleep(2000); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 */
+		
+		// Select by value
+		dropdown.selectByValue("option3");
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+		/*
+		 * try { Thread.sleep(2000); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 */
+		
+		// Find total number of options in dropdown
+		System.out.println("Total number of options in dropdown: " + dropdown.getOptions().size());
+		
+		driver.quit();
+        
+
+	}
+
+}
+
+
+
+// Dropdown having select tag in DOM
+// Dropdown does not have select tag in DOM (input/div) - Bootstrap dropdown
+// Auto suggest dropdown (Dynamic dropdown)
